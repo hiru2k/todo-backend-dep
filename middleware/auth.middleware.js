d592b08b7193573bb89224ab1106a8e16d5f5110
LassazVegaz <lasindunuwanga@gmail.com>
2023-01-17 20:42:33 +0530

    bearer token added

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,19 +1,20 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+	const bearerToken = req.headers.authorization;
 
-  if (!token) {
-    res.status(401).send("Unauthorized");
-  } else {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decoded.id;
-      next();
-    } catch (error) {
-      res.status(401).send("Unauthorized");
-    }
-  }
+	if (!bearerToken) {
+		res.status(401).send("Unauthorized");
+	} else {
+		try {
+			const token = bearerToken.split(" ")[1];
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+			req.userId = decoded.id;
+			next();
+		} catch (error) {
+			res.status(401).send("Unauthorized");
+		}
+	}
 };
 
 export default authMiddleware;
